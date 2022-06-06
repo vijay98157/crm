4ce27c071658b170195719a420e2dec8044bad55
Garima_QA <garima.singh@devkraft.in>
2022-06-06 13:08:05 +0530

    devkraftteam

--- crm/src/crm/orange.java
+++ crm/src/crm/orange.java
@@ -1,3 +1,4 @@
+
 package crm;
 
 public class orange {
@@ -5,6 +6,9 @@ public class orange {
 	public static void name() {
 		System.out.println("vijay");
 		System.out.println("kumar");
+		System.out.println("new team");
+		System.out.println("new team kumar");
+		System.out.println("teamdevkraft");
 	}
 
 }
