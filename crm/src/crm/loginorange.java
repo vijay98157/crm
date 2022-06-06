e327b0bf4335d1fbb4fb28a8ceb40eb40b4970a3
Garima_QA <garima.singh@devkraft.in>
2022-06-06 14:38:49 +0530

    sachin

---
+++ crm/src/crm/loginorange.java
@@ -0,0 +1,11 @@
+package crm;
+
+public class loginorange {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("sachin");
+
+	}
+
+}
--- crm/src/crm/orange.java
+++ crm/src/crm/orange.java
@@ -4,10 +4,7 @@ package crm;
 public class orange {
 
 	public static void name() {
-		System.out.println("vijay");
-		System.out.println("kumar");
-		System.out.println("new team");
-		System.out.println("new team kumar");
+
 		System.out.println("teamdevkraft");
 	}
 
